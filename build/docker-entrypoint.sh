878532e9a65fc1476bf27768914da8bbb6bc118e
adriandez <adriandez@gmail.com>
2024-08-19 17:52:15 +0200

    Fixed \n

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -2,31 +2,31 @@
 set -e
 
 # Echo environment variables for logging purposes (optional)
-echo "[TEST_ENV]: $TEST_ENV"
-echo "[VIEWPORT_WIDTH]: $VIEWPORT_WIDTH"
-echo "[VIEWPORT_HEIGHT]: $VIEWPORT_HEIGHT"
-echo "[TEST_BROWSER]: $TEST_BROWSER"
-echo "[ENABLE_VIDEO]: $ENABLE_VIDEO \n"
+echo -e "[TEST_ENV]: $TEST_ENV\n"
+echo -e "[VIEWPORT_WIDTH]: $VIEWPORT_WIDTH\n"
+echo -e "[VIEWPORT_HEIGHT]: $VIEWPORT_HEIGHT\n"
+echo -e "[TEST_BROWSER]: $TEST_BROWSER\n"
+echo -e "[ENABLE_VIDEO]: $ENABLE_VIDEO\n"
 
 # Ensure bash is available (optional)
-echo "[BASH] Checking if bash is available \n"
+echo -e "[BASH] Checking if bash is available\n"
 which bash
 
 # Ensure correct permissions for /tmp
-echo "[Permissions] Setting permissions for /tmp \n"
+echo -e "[Permissions] Setting permissions for /tmp\n"
 chmod 1777 /tmp
 
 # Unset XDG_RUNTIME_DIR to avoid dbus-launch issues
-echo "[XDG] Unsetting XDG_RUNTIME_DIR \n"
+echo -e "[XDG] Unsetting XDG_RUNTIME_DIR\n"
 unset XDG_RUNTIME_DIR
 
 # Start a new DBus session
-echo "[DBUS] Starting new DBus session"
+echo -e "[DBUS] Starting new DBus session\n"
 eval `dbus-launch --sh-syntax`
-echo "[DBUS] DBus session started with address $DBUS_SESSION_BUS_ADDRESS \n"
+echo -e "[DBUS] DBus session started with address $DBUS_SESSION_BUS_ADDRESS\n"
 
 # Kill any existing Xvfb processes (use pkill as an alternative)
-echo "[XVFB] Killing existing Xvfb processes (if any)"
+echo -e "[XVFB] Killing existing Xvfb processes (if any)\n"
 pkill -f Xvfb || true
 
 # Check if DISPLAY variable is set, otherwise default to :99
@@ -35,16 +35,16 @@ if [ -z "$DISPLAY" ]; then
 fi
 
 # Start Xvfb on the specified DISPLAY
-echo "[XVFB] Starting Xvfb on display $DISPLAY"
+echo -e "[XVFB] Starting Xvfb on display $DISPLAY\n"
 Xvfb $DISPLAY -screen 0 1920x1080x24 &
-echo "[XVFB] Xvfb started on $DISPLAY \n"
+echo -e "[XVFB] Xvfb started on $DISPLAY\n"
 
 # Wait for Xvfb to start
 sleep 5
 
 # Run the tests
-echo "[CYPRESS] Running Cypress tests with browser $TEST_BROWSER \n"
+echo -e "[CYPRESS] Running Cypress tests with browser $TEST_BROWSER\n"
 
 # Execute the command passed to the docker run command
-echo "[CMD] Executing command: $@"
+echo -e "[CMD] Executing command: $@\n"
 exec "$@"
